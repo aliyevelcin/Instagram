944367282d74cf1eb0b4908947778dba7a8e24c6
aliyevelcin <elcinaliyev706@gmail.com>
2023-04-24 16:27:25 +0400

    m

---
+++ core/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from core.views import mainpage , explore
+
+
+app_name = 'core'
+
+
+urlpatterns = [
+    path('',mainpage,name ='main'),
+    path('exp/',explore,name ='main-two'),
+
+]
---
+++ core/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from core.models import Post
+
+def mainpage(request):
+    posts = Post.objects.all()
+    context = {
+        'posts':posts
+    }
+    return render(request, 'insta.html', context)
+
+def explore(request):
+    posts = Post.objects.all()
+    context = {
+        'posts':posts
+    }
+    return render(request, 'explore.html', context)
+ 
+# Create your views here.
